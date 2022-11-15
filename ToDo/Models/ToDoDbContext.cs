2c3eeab6f27e32957f6e0f65b6fbebd20c7a6041
world.cyp <59548715+world137@users.noreply.github.com>
2022-11-15 13:44:02 +0700

    Update ToDoDbContext.cs

--- ToDo/Models/ToDoDbContext.cs
+++ ToDo/Models/ToDoDbContext.cs
@@ -24,7 +24,7 @@ namespace ToDo.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=World.$k137;database=ToDo", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.9.2-mariadb"));
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=;database=ToDo", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.9.2-mariadb"));
             }
         }
 
